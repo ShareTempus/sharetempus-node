950dbcff3b3be25e0a2a513949c18bd3a9615322
Lucas Bassetti <lucas@MBP-de-Lucas.home>
2017-03-29 22:18:40 -0300

    add customers find endpoint

--- spec/categories.spec.js
+++ spec/categories.spec.js
@@ -2,12 +2,12 @@ var ShareTempus = require('../lib/ShareTempus')('sk_test_wCPyemzK7r6ZYaiRkEqzNvF
 
 describe("Categories", function() {
 
-    it("Should retrieve the categories", (done) => {
-        ShareTempus.categories.retrieve(function(err, categories) {
-            expect(categories).not.toBeUndefined();
-            expect(typeof categories).toBe('object');
-            expect(categories['Toys and Games'].length).toBeGreaterThan(1);
-            done();
-        });
+  it("Should retrieve the categories", (done) => {
+    ShareTempus.categories.retrieve(function(err, categories) {
+      expect(categories).not.toBeUndefined();
+      expect(typeof categories).toBe('object');
+      expect(categories['Toys and Games'].length).toBeGreaterThan(1);
+      done();
     });
+  });
 });
--- spec/customer.spec.js
+++ spec/customer.spec.js
@@ -2,44 +2,44 @@ var ShareTempus = require('../lib/ShareTempus')('sk_test_wCPyemzK7r6ZYaiRkEqzNvF
 
 describe("Customers", function() {
 
-    it("Should be return a customer not found error", (done) => {
-        ShareTempus.customers.retrieve({
-            customer: 'cus_dD2r2Ib8kPMhg5C3YvlqRw'
-        }, (err, customer) => {
-            expect(err.type).toBe('STInvalidRequestError');
-            expect(err.message).toBe('Customer not found');
-            expect(customer).toBeUndefined();
-            done();
-        }).catch((err) => {
-            expect(err.type).toBe('STInvalidRequestError');
-            expect(err.message).toBe('Customer not found');
-            done();
-        });
+  it("Should be return a customer not found error", (done) => {
+    ShareTempus.customers.retrieve({
+      customer: 'cus_dD2r2Ib8kPMhg5C3YvlqRw'
+    }, (err, customer) => {
+      expect(err.type).toBe('STInvalidRequestError');
+      expect(err.message).toBe('Customer not found');
+      expect(customer).toBeUndefined();
+      done();
+    }).catch((err) => {
+      expect(err.type).toBe('STInvalidRequestError');
+      expect(err.message).toBe('Customer not found');
+      done();
     });
+  });
 
-    it("Should update a customer", (done) => {
-        ShareTempus.customers.update({
-            id: 'cus_dD2r2Ib8kPMhg5C3YvlqRwb7',
-            legalEntity: {
-                ssnLast4: '4321'
-            }
-        }, (err, customer) => {
-            expect(customer).not.toBeUndefined();
-            expect(typeof customer).toBe('object');
-            expect(customer.legalEntity.ssnLast4).toBe('4321');
-            done();
-        });
+  it("Should update a customer", (done) => {
+    ShareTempus.customers.update({
+      id: 'cus_dD2r2Ib8kPMhg5C3YvlqRwb7',
+      legalEntity: {
+        ssnLast4: '4321'
+      }
+    }, (err, customer) => {
+      expect(customer).not.toBeUndefined();
+      expect(typeof customer).toBe('object');
+      expect(customer.legalEntity.ssnLast4).toBe('4321');
+      done();
     });
+  });
 
-    it("Should retrieve a customer", (done) => {
-        ShareTempus.customers.retrieve({
-            customer: 'cus_dD2r2Ib8kPMhg5C3YvlqRwb7'
-        }, (err, customer) => {
-            expect(customer).not.toBeUndefined();
-            expect(typeof customer).toBe('object');
-            expect(customer.email).toBe('customer@test.com');
-            done();
-        });
+  it("Should retrieve a customer", (done) => {
+    ShareTempus.customers.retrieve({
+      customer: 'cus_dD2r2Ib8kPMhg5C3YvlqRwb7'
+    }, (err, customer) => {
+      expect(customer).not.toBeUndefined();
+      expect(typeof customer).toBe('object');
+      expect(customer.email).toBe('customer@test.com');
+      done();
     });
+  });
 
 });
