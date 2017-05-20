d6d81aae8fed8808e9c87814261283e1d976e3dc
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2017-05-20 14:53:17 -0300

    add tests

--- tests/customer.spec.js
+++ tests/customer.spec.js
@@ -11,33 +11,21 @@ const ShareTempus = require('./config');
 
 describe('Customers', () => {
   let stubedCreate;
+  let stubedUpdate;
+  let stubedFind;
   let stubedRetrieve;
-  const customer = {
-    email: 'email@test.com',
-    legalEntity: {
-      type: 'individual',
-      firstName: 'Trenton',
-      lastName: 'Large',
-      birthdate: 637124400000,
-      ssnLast4: '1234',
-      address: {
-        city: 'New York City',
-        country: 'US',
-        line1: 'East 169th Street',
-        line2: 'Apt. 2A Bronx',
-        postalCode: '10456',
-        state: 'New York',
-      },
-    },
-  };
 
   beforeEach(() => {
     stubedCreate = sinon.stub(ShareTempus.customers, 'create');
+    stubedUpdate = sinon.stub(ShareTempus.customers, 'update');
+    stubedFind = sinon.stub(ShareTempus.customers, 'find');
     stubedRetrieve = sinon.stub(ShareTempus.customers, 'retrieve');
   });
 
   afterEach(() => {
     stubedCreate.restore();
+    stubedUpdate.restore();
+    stubedFind.restore();
     stubedRetrieve.restore();
   });
 
@@ -45,19 +33,69 @@ describe('Customers', () => {
     expect(ShareTempus.customers.create).to.exist;
   });
 
+  it('should have update method', () => {
+    expect(ShareTempus.customers.update).to.exist;
+  });
+
+  it('should have find method', () => {
+    expect(ShareTempus.customers.update).to.exist;
+  });
+
   it('should have retrieve method', () => {
     expect(ShareTempus.customers.retrieve).to.exist;
   });
 
   it('should create a customers', () => {
+    const customer = {
+      email: 'email@test.com',
+      legalEntity: {
+        type: 'individual',
+        firstName: 'Trenton',
+        lastName: 'Large',
+        birthdate: 637124400000,
+        ssnLast4: '1234',
+        address: {
+          city: 'New York City',
+          country: 'US',
+          line1: 'East 169th Street',
+          line2: 'Apt. 2A Bronx',
+          postalCode: '10456',
+          state: 'New York',
+        },
+      },
+    };
+
     ShareTempus.customers.create(customer);
     expect(stubedCreate).to.have.been.calledOnce;
     expect(stubedCreate).to.have.been.calledWith(customer);
   });
 
+  it('should update a customers', () => {
+    const customer = {
+      id: 'cus_dD2r2Ib8kPMhg5C3YvlqRwb7',
+      legalEntity: {
+        address: {
+          line2: 'Apt. 1A Bronx',
+        },
+      },
+    };
+
+    ShareTempus.customers.update(customer);
+    expect(stubedUpdate).to.have.been.calledOnce;
+    expect(stubedUpdate).to.have.been.calledWith(customer);
+  });
+
+  it('should find a customer', () => {
+    const customer = { email: 'email@test.com' };
+    ShareTempus.customers.find(customer);
+    expect(stubedFind).to.have.been.calledOnce;
+    expect(stubedFind).to.have.been.calledWith(customer);
+  });
+
   it('should retrieve a customer', () => {
-    ShareTempus.customers.retrieve({ customer: 'cus_oC3ImnDw1Iqw1b3sx5CITtbc' });
+    const customer = { customer: 'cus_oC3ImnDw1Iqw1b3sx5CITtbc' };
+    ShareTempus.customers.retrieve(customer);
     expect(stubedRetrieve).to.have.been.calledOnce;
-    expect(stubedRetrieve).to.have.been.calledWith({ customer: 'cus_oC3ImnDw1Iqw1b3sx5CITtbc' });
+    expect(stubedRetrieve).to.have.been.calledWith(customer);
   });
 });
